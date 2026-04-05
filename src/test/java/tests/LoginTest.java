dc14bd5d1428bad09d826e312dbe4bda33bdab47
natalia_iunusova <yunusova.nv@gmail.com>
2026-04-05 22:32:18 +0300

    add BaseTest

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+
+
+    @Test
+    public void checkLogin() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("[id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        String title = driver.findElement(By.cssSelector("[data-test='title']")).getText();
+        assertEquals(title, "Products");
+    }
+
+    @Test
+    public void checkIncorrectLogin() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("[id='user-name']")).sendKeys("locked_out_user");
+        driver.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        boolean isErrorMsDisplayed = driver.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
+        assertTrue(isErrorMsDisplayed, "The error message fails to appear");
+        String title = driver.findElement(By.xpath("//h3[contains(text(), 'Epic sadface')]")).getText();
+        assertEquals(title, "Epic sadface: Sorry, this user has been locked out.");
+    }
+}
