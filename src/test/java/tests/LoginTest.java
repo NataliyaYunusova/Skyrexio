e4f2862940a973702a9885f4646a58609cd28ee0
natalia_iunusova <yunusova.nv@gmail.com>
2026-04-25 18:05:31 +0300

    added User and UserFactory

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,24 +2,27 @@ package tests;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import user.User;
+import user.UserFactory;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static user.UserFactory.withAdminPermission;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void checkLogin() {
         loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
+        loginPage.login(withAdminPermission());
 
         assertEquals(productsPage.getTitle(), "Products");
     }
 
     @Test(dataProvider = "incorrectData")
-    public void checkLockedUserIncorrectLogin(String user, String password, String errorMessage) {
+    public void checkLockedUserIncorrectLogin(User user, String errorMessage) {
         loginPage.open();
-        loginPage.login(user, password);
+        loginPage.login(user);
 
         assertTrue(loginPage.isErrorMsDisplayed(), "The error message fails to appear");
         assertEquals(loginPage.getErrorMessage(), errorMessage);
@@ -28,10 +31,10 @@ public class LoginTest extends BaseTest {
     @DataProvider(name = "incorrectData")
     public Object[][] loginData() {
         return new Object[][]{
-                {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."},
-                {"", "secret_sauce", "Epic sadface: Username is required"},
-                {"standard_user", "", "Epic sadface: Password is required"},
-                {"Standard_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"}
+                {UserFactory.withLockedPermission(), "Epic sadface: Sorry, this user has been locked out."},
+                {new User("", "secret_sauce"), "Epic sadface: Username is required"},
+                {new User("standard_user", ""), "Epic sadface: Password is required"},
+                {UserFactory.withIncorrectPermission(), "Epic sadface: Username and password do not match any user in this service"}
         };
     }
 }
